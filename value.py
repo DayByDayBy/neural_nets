6f48522fa114a70e8c25f88e16a5e6b5d54f1760
Diomedes Git <diomedesgit@gmail.com>
2024-11-11 00:04:50 +0000

    discrete value component with mul

--- value.py
+++ value.py
@@ -19,3 +19,14 @@ class Value:
         out._backward = _backward
         out._prev = {self, other}
         return out
+    
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data)
+        
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        out._prev = {self, other}
+        return out  
